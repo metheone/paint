9cfb38cd305fb39dd033e44502e5edd531d9296f
saeidtheone <saeidjanani@yahoo.com>
2016-06-05 17:18:47 -0700

    more updates

--- PaintDesktopConsole/ManufacturerForm.cs
+++ PaintDesktopConsole/ManufacturerForm.cs
@@ -16,6 +16,7 @@ namespace PaintDesktopConsole
     {
         private Main _mainForm = null;
         private readonly IPaintService _paintService;
+        private List<Manufacturer> _list;
 
         public ManufacturerForm()
         {
@@ -32,13 +33,15 @@ namespace PaintDesktopConsole
         {
             // Gets all the manufactuerer list items.
             //TODO: WE can use pictureBox and use forloop and for each manufactuers instatiate one picturebox.
-            List<Manufacturer> list = _paintService.GetAllManufacturers().ToList();
+            _list = _paintService.GetAllManufacturers().ToList();
+            button1.Text = _list.FirstOrDefault().Name;
         }
+
 
          private void button1_Click(object sender, EventArgs e)
         {
             //SElected Manufacturer
-            this._mainForm.CurrentPartLog.ManufacturerId = 100;
+            this._mainForm.CurrentPartLog.ManufacturerId = _list.FirstOrDefault().ManufacturerId;// This is just testing. The ID should come from what user selected
             this.Close();
             this._mainForm.UpdatedMainForm();
             this._mainForm.Focus();
